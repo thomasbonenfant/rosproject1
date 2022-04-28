5e1c0239dd7903f3a2e225742d550f17831679d9
VeronicaCardigliano <10627267@polimi.it>
2022-04-28 01:37:22 -0700

    one single file for each subscriber

--- src/vel_sub.cpp
+++ src/vel_sub.cpp
@@ -1,5 +1,76 @@
 #include "ros/ros.h"
-#include "rosproject1/VelSubscriber.h"
+#include "geometry_msgs/TwistStamped.h"
+#include "geometry_msgs/Vector3.h"
+#include <sstream>
+
+#include "rosproject1/WheelsSpeeds.h"
+
+class VelSubscriber {
+public:
+	static constexpr float radius = 0.07; //radius of wheels
+  static constexpr double l = 0.2; //wheel position along x (+-l)
+  static constexpr double w = 0.169; //wheel position along y (+-w)
+  static constexpr double T = 5; //gear ratio
+  static constexpr double multFactor = (60 * T) / radius;
+  
+  VelSubscriber() {
+  	//this specifies also in which class the callback is defined
+	  this->sub = this->n.subscribe("cmd_vel", 1000, &VelSubscriber::wheelsCallback, this);
+	  this->wheels_pub = this->n.advertise<rosproject1::WheelsSpeeds>("wheels_rpm", 1000);
+  } 
+
+  void main_loop() {
+  	ros::Rate loop_rate(10);
+
+		while (ros::ok()) {
+			ros::spinOnce();
+			loop_rate.sleep();
+		}
+  }
+
+  // this callback is gonna publish wheels velocities
+	void wheelsCallback(const geometry_msgs::TwistStamped::ConstPtr& msg) {
+		std::stringstream ss;
+		ss << "I heard timestamp sec: " << msg->header.stamp.sec << std::endl;
+	  ss << "and nanosec: " << msg->header.stamp.nsec << std::endl;
+	  
+		//create a new message of type WheelsSpeeds to be filled
+		rosproject1::WheelsSpeeds speeds_msg;
+
+		//setting the header
+		//speeds_msg.header.seq = msg->header.seq;
+		ss << "I refer to seq: " << msg->header.seq << std::endl;
+		speeds_msg.header.stamp = msg->header.stamp;
+		speeds_msg.header.frame_id = msg->header.frame_id;
+
+		ss << "Received omegaz: " << msg->twist.angular.z << std::endl;
+	  ss << "Received vx: " << msg->twist.linear.x << std::endl;
+	  ss << "Received vy: " << msg->twist.linear.y << std::endl;
+
+		speeds_msg.rpm_fl = (((- VelSubscriber::l - VelSubscriber::w) * msg->twist.angular.z) + 
+		msg->twist.linear.x - msg->twist.linear.y) * VelSubscriber::multFactor;
+		speeds_msg.rpm_fr = (((VelSubscriber::l + VelSubscriber::w) * msg->twist.angular.z) + 
+		msg->twist.linear.x + msg->twist.linear.y) * VelSubscriber::multFactor;
+		speeds_msg.rpm_rl = (((VelSubscriber::l + VelSubscriber::w) * msg->twist.angular.z) + 
+		msg->twist.linear.x - msg->twist.linear.y) * VelSubscriber::multFactor;
+		speeds_msg.rpm_rr = (((- VelSubscriber::l - VelSubscriber::w) * msg->twist.angular.z) + 
+		msg->twist.linear.x + msg->twist.linear.y) * VelSubscriber::multFactor;
+
+		ss << "Computed rpm for front left wheel: " << speeds_msg.rpm_fl << std::endl;
+	  ss << "Computed rpm for front right wheel: " << speeds_msg.rpm_fr << std::endl;
+	  ss << "Computed rpm for rear left wheel: " << speeds_msg.rpm_rl << std::endl;
+	  ss << "Computed rpm for rear right wheel: " << speeds_msg.rpm_rr << std::endl;
+	  ROS_INFO_STREAM(ss.str());
+
+	  this->wheels_pub.publish(speeds_msg);
+	}
+
+private:
+  //member variables
+  ros::NodeHandle n;
+  ros::Subscriber sub;
+  ros::Publisher wheels_pub;
+};
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "vel_sub");
--- src/wheels_sub.cpp
+++ src/wheels_sub.cpp
@@ -1,5 +1,156 @@
 #include "ros/ros.h"
-#include "rosproject1/WheelsSubscriber.h"
+#include "std_msgs/String.h"
+#include "sensor_msgs/JointState.h"
+//useful link: https://la.mathworks.com/help/ros/ug/work-with-basic-ros-messages.html
+#include "geometry_msgs/TwistStamped.h"
+#include "geometry_msgs/Vector3.h"
+#include <sstream>
+
+class WheelsSubscriber {
+public:
+
+  static constexpr float radius = 0.07; //radius of wheels
+  static constexpr int N = 42; //encoder CPR
+  static constexpr double T = 5; //gear ratio
+  static constexpr double l = 0.2; //wheel position along x (+-l)
+  static constexpr double w = 0.169; //wheel position along y (+-w)
+  static constexpr double multFactor = (1/(N*T)) * 2 * M_PI;
+
+  WheelsSubscriber() {
+    //this specifies also in which class the callback is defined
+    this->sub = this->n.subscribe("wheel_states", 1000, &WheelsSubscriber::velCallback, this);
+    this->vel_pub = this->n.advertise<geometry_msgs::TwistStamped>("cmd_vel", 1000);
+  }
+
+  void main_loop() {
+    ros::Rate loop_rate(10);
+
+    this->prev_stamp = ros::Time(0);
+
+    while (ros::ok()) {
+      ros::spinOnce();
+      loop_rate.sleep();
+    }
+  }
+  void velCallback(const sensor_msgs::JointState::ConstPtr& msg) {
+    //code to print received message infos
+  std::string name;
+  for (auto i: msg->name) {
+      name.append(i.c_str());
+    name.append(" ");
+  }
+  std::stringstream ss;
+  ss << std::setprecision(3); //to see only 3 decimals
+  ss << "I heard timestamp sec: " << msg->header.stamp.sec << std::endl;
+  ss << "and nanosec: " << msg->header.stamp.nsec << std::endl;
+  ss << "wheels: " << name.c_str() << std::endl;
+  ss << "position: ";
+  std::copy(msg->position.begin(), msg->position.end(), std::ostream_iterator<double>(ss, " "));
+  ss << std::endl;
+  ss << "velocity: ";
+  std::copy(msg->velocity.begin(), msg->velocity.end(), std::ostream_iterator<double>(ss, " "));
+  ss << std::endl;
+  ROS_INFO_STREAM(ss.str());
+
+  //also time expressed in double to be compatible with position
+  geometry_msgs::TwistStamped vel_msg;
+  double deltaTime;
+  std::vector<double> deltaTicks;
+  std::vector<double> wheelsVel(4);
+  
+  if (this->prev_stamp != ros::Time(0)) {
+
+    ss.str("");
+    ss << "Current seq: " << msg->header.seq << std::endl;
+
+    // computation of deltaTime in nsec
+    if (this->prev_stamp.sec == msg->header.stamp.sec) {
+      deltaTime = msg->header.stamp.nsec - this->prev_stamp.nsec;
+    }
+    else
+      deltaTime = (1000000000 - this->prev_stamp.nsec) + msg->header.stamp.nsec;
+    if (msg->header.stamp.sec - this->prev_stamp.sec > 1)
+      ROS_INFO("WARNING: difference of time is more than one second ");
+
+    ss << "Computed deltaTime in nsec: " << deltaTime << std::endl;
+
+    // conversion of deltaTime from nsec to sec
+    deltaTime = deltaTime/1000000000;
+
+    ss << "Computed deltaTime in sec: " << deltaTime << std::endl;
+    
+    // computation of deltaTicks
+    std::transform((msg->position).begin(), (msg->position).end(), (this->prev_position).begin(), 
+      std::back_inserter(deltaTicks), [](double l, double r) { return fabs(l - r);});
+
+    ss << "Computed deltaTicks 1: " << deltaTicks.at(0) << std::endl;
+    ss << "From new position: " << msg->position.at(0) << std::endl;
+    ss << "And prev position: " << this->prev_position.at(0) << std::endl;
+
+    ss << "Computed deltaTicks 2: " << deltaTicks.at(1) << std::endl;
+    ss << "From new position: " << msg->position.at(1) << std::endl;
+    ss << "And prev position: " << this->prev_position.at(1) << std::endl;
+    
+    // dividing deltaTicks for deltaTime
+    transform(deltaTicks.begin(), deltaTicks.end(), deltaTicks.begin(), 
+      [deltaTime](double &c){ return c/deltaTime;});
+
+    ss << "DeltaTicks/DeltaTime: " << deltaTicks.at(0) << std::endl;
+    
+    // finding wheels velocity multiplying deltaTicks with multFactor
+    wheelsVel = deltaTicks;
+    double mult = WheelsSubscriber::multFactor;
+    transform(wheelsVel.begin(), wheelsVel.end(), wheelsVel.begin(), 
+      [mult](double &c){ return c*mult;});
+
+    ss << "WheelVel 1: " <<wheelsVel.at(0) << std::endl;
+    ss << "WheelVel 2: " <<wheelsVel.at(1) << std::endl;
+    ss << "WheelVel 3: " <<wheelsVel.at(2) << std::endl;
+    ss << "WheelVel 4: " <<wheelsVel.at(3) << std::endl;
+
+    // computing linear and angular speeds from given system info and wheels velocities
+    
+    this->angular.z = (wheelsVel.at(1) - wheelsVel.at(2) + wheelsVel.at(3) - wheelsVel.at(0))*
+    WheelsSubscriber::radius/(4*(WheelsSubscriber::l + WheelsSubscriber::w));
+    this->linear.x = (wheelsVel.at(0) + wheelsVel.at(1) + wheelsVel.at(2) + wheelsVel.at(3))*WheelsSubscriber::radius/4;
+    this->linear.y = (wheelsVel.at(1) + wheelsVel.at(2) - wheelsVel.at(3)- wheelsVel.at(0))*WheelsSubscriber::radius/4;
+    
+    //set values
+    vel_msg.twist.linear = this->linear;
+    vel_msg.twist.angular = this->angular;
+    
+    //set the header
+    //vel_msg.header.seq = msg->header.seq;
+    
+    vel_msg.header.frame_id = msg->header.frame_id;
+    //when we reach a timestamp in the msg, we should use it
+    //also for the published msg as it refers also to that time
+    vel_msg.header.stamp = msg->header.stamp;
+    
+    ss << "The computed linear velocity is: " << vel_msg.twist.linear.x << " along x axis," << std::endl;
+    ss << vel_msg.twist.linear.y << " along y axis," << std::endl;
+    ss << vel_msg.twist.linear.z << " along z axis," << std::endl;
+    ss << "The computed angular velocity is: " << vel_msg.twist.angular.x << " along x axis," << std::endl;
+    ss << vel_msg.twist.angular.y << " along y axis," << std::endl;
+    ss << vel_msg.twist.angular.z << " along z axis" << std::endl;
+    ROS_INFO_STREAM(ss.str());
+    
+    this->vel_pub.publish(vel_msg);
+  }
+  this->prev_stamp = msg->header.stamp;
+  this->prev_position = msg->position;
+  }
+   
+private:
+  //member variables, visible only IN the class
+  ros::NodeHandle n;
+  ros::Subscriber sub;
+  ros::Publisher vel_pub;
+  geometry_msgs::Vector3 linear;
+  geometry_msgs::Vector3 angular;
+  ros::Time prev_stamp;
+  std::vector<double> prev_position;
+};
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "wheels_sub");
